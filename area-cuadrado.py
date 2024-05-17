cbb540c7f6e26d47bc80e805e6bb479bf41a1c4c
Baranov <114580386+baranov7@users.noreply.github.com>
2024-05-17 05:08:39 -0400

    Add files via upload

---
+++ area-cuadrado.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario la longitud del lado.
+lado = int(input("Introduzca la longitud del lado: "))
+
+# Calcular el área
+area = lado * lado
+
+# Mostrar el resultado en consola.
+print(f"El área de un cuadrado con el lado {lado}, es: {area}.")
